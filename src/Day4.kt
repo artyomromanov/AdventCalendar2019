fdd5f12c53b9e45cdd99994fa353c03a8f834171
berryfurious <artyomvromanov@gmail.com>
2020-05-22 00:50:27 +0100

    quick optimisation

--- src/Day4.kt
+++ src/Day4.kt
@@ -2,41 +2,53 @@ import java.util.function.Predicate
 
 val passwordRange = 158126..624574
 
-
 class PasswordGenerator {
 
-    private val predicateTwoConsecutiveNumbers = Predicate<Int> {
+    private val predicate = Predicate<Int> {
 
         var previousNum = -1
-        var hasConsecutiveTwo = 0 //Zero for no double occurence, and -1 for larger than two group of the same number
+        var sequenceTracker = 0
+        var doublePredicateFulfilled = false
 
-        it.toString().forEachIndexed { numberIndex, number ->
-            var num = number.toInt()
-            when {
-                num == previousNum -> {
-                    if(num == hasConsecutiveTwo){
-                        //third occurence(or more)
-                        hasConsecutiveTwo = -1
-                    }else{
-                        if(hasConsecutiveTwo == 0 || hasConsecutiveTwo != -1){
-                            //first occurence || replacing sequence
-                            hasConsecutiveTwo = num
+        it.toString().forEach { number ->
+
+            val num = number.toInt()
+            //Increasing predicate
+            if (num < previousNum) {
+                return@Predicate false
+            }
+
+            //Predicate of two, but not more consecutive numbers
+            if (!doublePredicateFulfilled) {
+                //If number is equal to his preceding number
+                if (num == previousNum) {
+                    //See if number is equals to stored tracker value(init = 0; when sequence > 3 in a row = -1
+                    if (num == sequenceTracker) { //if number in a sequence is the same that was previously stored
+                        sequenceTracker = -1 //sets the sequence as not valid for predicate, as it is longer than 2 characters
+                    } else {
+                        if (sequenceTracker != -1) { //otherwise, as long as number stored is not indicating an invalid sequence,
+                            sequenceTracker = num   //store this new number, as this is a new sequence
                         }
                     }
-                }
-                num < previousNum -> {
-                    return@Predicate false
+                } else {
+                    if (sequenceTracker != 0 && sequenceTracker != -1) { //if after two equal numbers stored tracker is valid
+                        doublePredicateFulfilled = true  //this predicate is fulfilled, regardless of the remaining digits
+                    }
+                    sequenceTracker = 0 //in any case, reset the tracker
                 }
             }
-            previousNum = num
+            previousNum = num //save this number to become the previous number in the next iteration
         }
-        return@Predicate hasConsecutiveTwo != -1 && hasConsecutiveTwo != 0
+        if (sequenceTracker != 0 && sequenceTracker != -1) doublePredicateFulfilled = true
+        //reassess doublePredicate as if it is fulfilled with the last digit, it must be set to true
+        return@Predicate doublePredicateFulfilled
     }
+
 
     fun generate() {
         passwordRange
-            .filter { predicateTwoConsecutiveNumbers.test(it) }
+            .filter { predicate.test(it) }
             .also { println(it.size) }
-        //.forEach { println(it) } //prints all the elements of the list
+            //.forEach { println(it) } //prints all the elements of the list
     }
 }
