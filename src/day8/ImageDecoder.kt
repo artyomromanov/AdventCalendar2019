a9140c4625bb415b83893364a0c062c8192f752d
berryfurious <artyomvromanov@gmail.com>
2020-06-08 01:14:02 +0100

    image decoded

--- src/day8/ImageDecoder.kt
+++ src/day8/ImageDecoder.kt
@@ -8,39 +8,59 @@ const val columns = 25
 
 object ImageDecoder {
 
-    private var imageMatrix = readImageData()
-    var valuesList = mutableListOf<Map<Int, List<Int>>>()
+    private var imageMatrix = decodeImageData()
+    private var valuesList = mutableListOf<Map<Int, List<Int>>>()
 
     //Prints the layers of the image
     fun calculateImageData(): MutableList<Map<Int, List<Int>>> {
         imageMatrix.forEach { layer ->
-            val valuesMap : Map<Int, List<Int>> = layer
+            val valuesMap: Map<Int, List<Int>> = layer
                 .flatten()
                 .groupBy { it }
                 .toSortedMap()
             valuesList.add(valuesMap)
         }
-        return valuesList.also { list -> list.sortBy { it[0]?.size } }
+        valuesList.sortBy { it[0]?.size }
+
+        println("Layer 0 contains fewest amount of zeroes.")
+        println("Amount of 1s * 2s on that level is ${ImageDecoder.valuesList.first()[1]?.size} * ${ImageDecoder.valuesList.first()[2]?.size}")
+        println("Answer is :  ${ImageDecoder.valuesList.first()[1]?.size!!.times(ImageDecoder.valuesList.first()[2]?.size!!)}")
+
+        return valuesList
+
     }
 
     //Initialize the 3D array of the image layers
-    private fun readImageData(): MutableList<MutableList<List<Int>>> {
+    private fun decodeImageData(): MutableList<MutableList<List<Int>>> {
 
         val data = File(filepath).readText().chunked(columns * rows)
         val matrix: MutableList<MutableList<List<Int>>> = mutableListOf()
+        val decodedImage = mutableMapOf<Int, Int>()
 
         data.forEach { layer ->
             val rows: MutableList<List<Int>> = mutableListOf()
             layer
                 .chunked(columns)
-                .forEach { row ->
+                .forEachIndexed { yPos, row ->
+                    var xPos = 0
                     rows.add(row.map {
                         val value = it.toString().toInt()
+                        val coordIndex = xPos + (columns * yPos)
+                        if (value != 2) decodedImage.putIfAbsent(coordIndex, value)
+                        xPos++
                         value
                     })
                 }
             matrix.add(rows)
         }
+        printImage(decodedImage)
         return matrix
+    }
+
+    private fun printImage(decodedImage: MutableMap<Int, Int>) {
+        decodedImage
+            .toSortedMap()
+            .values.chunked(columns)
+            .forEach { println(it) }
     }
 }
